5419c5c315472f2ab029a1556ad5b14818e12e18
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2020-05-28 10:41:36 +0200

    .

---
+++ html/games/Achtung Kurve/src/js/lib/preferences/PreferenceWithValue.js
@@ -0,0 +1,9 @@
+export class PreferenceWithValue {
+    constructor(preference, value) {
+        if (!preference.isValidValue(value)) {
+            throw new TypeError(`${value} is not a valid value for preference ${preference.key}.`);
+        }
+        this.preference = preference;
+        this.value = value;
+    }
+}
